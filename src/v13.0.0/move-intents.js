09e1e1e2c40e74d1831ed81ea6b44458e55ff451
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:35:05 -0700

    feat(13.0.0): add `move-intents` transformer

---
+++ src/v13.0.0/move-intents.js
@@ -0,0 +1,48 @@
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	return root
+		.find(j.NewExpression, {
+			arguments: [
+				{
+					type: 'ObjectExpression',
+					properties: [
+						{
+							key: {
+								name: 'ws',
+							},
+							value: {
+								properties: [
+									{
+										key: {
+											name: 'intents',
+										},
+									},
+								],
+							},
+						},
+					],
+				},
+			],
+		})
+		.replaceWith(({ node }) => {
+			const clientProps = node.arguments[0].properties;
+			const ws = clientProps.find((prop) => prop.key.name === 'ws');
+			const wsProps = ws.value.properties;
+			const intents = wsProps.find((prop) => prop.key.name === 'intents');
+
+			node.arguments[0].properties.push(intents);
+			wsProps.splice(wsProps.indexOf(intents), 1);
+
+			if (wsProps.length === 0) clientProps.splice(clientProps.indexOf(ws), 1);
+
+			return node;
+		})
+		.toSource(printOptions);
+};
