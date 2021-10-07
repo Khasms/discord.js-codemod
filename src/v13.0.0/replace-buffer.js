5bd90d9bbd4b91406788c4bfbecfd157458a0032
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:46:19 -0700

    feat(13.0.0): add `replace-buffer` transformer

---
+++ src/v13.0.0/replace-buffer.js
@@ -0,0 +1,18 @@
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	root
+		.find(j.CallExpression)
+		.filter(({ node }) => ['convertToBuffer', 'str2ab'].includes(node.callee.property?.name))
+		.forEach(({ node }) => {
+			node.callee = j.memberExpression(j.identifier('Buffer'), j.identifier('from'));
+		});
+
+	return root.toSource(printOptions);
+};
