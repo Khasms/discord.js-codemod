6c8ee981848cff5025f11bf1cdbabe145074c8d9
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:36:20 -0700

    feat(13.0.0): add `move-webhook-client-params` transformer

---
+++ src/v13.0.0/move-webhook-client-params.js
@@ -0,0 +1,47 @@
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	return root
+		.find(j.NewExpression)
+		.filter(({ node }) => {
+			return (
+				(node.callee.name === 'WebhookClient' || node.callee.property?.name === 'WebhookClient') &&
+				node.arguments.length > 0
+			);
+		})
+		.replaceWith(({ node }) => {
+			const newNode = [j.objectExpression([])];
+
+			const id = node.arguments[0];
+			if (id.name === 'id') {
+				newNode[0].properties.push(id);
+			} else {
+				const idProp = j.property('init', j.identifier('id'), id);
+				newNode[0].properties.push(idProp);
+			}
+
+			const token = node.arguments[1];
+			if (token) {
+				if (token.name === 'token') {
+					newNode[0].properties.push(token);
+				} else {
+					const tokenProp = j.property('init', j.identifier('token'), token);
+					newNode[0].properties.push(tokenProp);
+				}
+			}
+
+			const options = node.arguments[2];
+			if (options) newNode.push(options);
+
+			node.arguments = newNode;
+
+			return node;
+		})
+		.toSource(printOptions);
+};
