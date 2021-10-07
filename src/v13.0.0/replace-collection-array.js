0e0790d0e955d1f9091a6ca4b20359aac732229b
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:47:43 -0700

    feat(13.0.0): add `replace-collection-array` transformer

---
+++ src/v13.0.0/replace-collection-array.js
@@ -0,0 +1,52 @@
+const { constructName } = require('../util');
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	// Replace Collection#array()
+	root
+		.find(j.CallExpression, {
+			callee: {
+				property: {
+					name: 'array',
+				},
+			},
+		})
+		.replaceWith(({ node }) => {
+			return j.arrayExpression([
+				j.spreadElement(
+					j.callExpression(
+						j.memberExpression(j.identifier(constructName(node.callee.object)), j.identifier('values')),
+						[],
+					),
+				),
+			]);
+		});
+
+	// Replace Collection#keyArray()
+	root
+		.find(j.CallExpression, {
+			callee: {
+				property: {
+					name: 'keyArray',
+				},
+			},
+		})
+		.replaceWith(({ node }) => {
+			return j.arrayExpression([
+				j.spreadElement(
+					j.callExpression(
+						j.memberExpression(j.identifier(constructName(node.callee.object)), j.identifier('keys')),
+						[],
+					),
+				),
+			]);
+		});
+
+	return root.toSource(printOptions);
+};
