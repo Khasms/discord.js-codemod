d616ce8bb2c10a3470eb77e71b41dd88b27bc6f7
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:53:39 -0700

    feat(13.0.0): add `update-fetch-invites` transformer

---
+++ src/v13.0.0/update-fetch-invites.js
@@ -0,0 +1,22 @@
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	return root
+		.find(j.CallExpression, {
+			callee: {
+				property: {
+					name: 'fetchInvites',
+				},
+			},
+		})
+		.forEach(({ node }) => {
+			node.callee.property.name = 'invites.fetch';
+		})
+		.toSource(printOptions);
+};
