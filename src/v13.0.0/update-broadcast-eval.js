f552e111c2919ea0e9c7ab883b9174e4b3356f5e
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:50:24 -0700

    feat(13.0.0): add `update-broadcast-eval` transformer

---
+++ src/v13.0.0/update-broadcast-eval.js
@@ -0,0 +1,30 @@
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	return root
+		.find(j.CallExpression, {
+			callee: {
+				property: {
+					name: 'broadcastEval',
+				},
+			},
+			arguments: [
+				{
+					type: 'Literal',
+				},
+			],
+		})
+		.forEach(({ node }) => {
+			node.arguments[0] = j.arrowFunctionExpression(
+				[j.identifier('client')],
+				node.arguments[0].value.replaceAll('this', 'client'),
+			);
+		})
+		.toSource(printOptions);
+};
