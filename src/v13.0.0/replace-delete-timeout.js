f372437deb845bc0f484d26df42f76273b309996
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-06 19:48:11 -0700

    feat(13.0.0): add `replace-delete-timeout` transformer

---
+++ src/v13.0.0/replace-delete-timeout.js
@@ -0,0 +1,29 @@
+const { parser } = require('../util');
+module.exports.parser = parser;
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	return root
+		.find(j.CallExpression, {
+			callee: {
+				property: {
+					name: 'delete',
+				},
+			},
+		})
+		.filter(({ node }) => {
+			return node.arguments[node.arguments.length > 1 ? 1 : 0]?.properties.some((prop) => prop.key.name === 'timeout');
+		})
+		.replaceWith(({ node }) => {
+			const timeoutIndex = node.arguments.length > 1 ? 1 : 0;
+			const timeout = node.arguments[timeoutIndex].properties.find((prop) => prop.key.name === 'timeout').value.value;
+			timeoutIndex ? node.arguments.splice(timeoutIndex, 1) : (node.arguments = []);
+
+			return j.callExpression(j.identifier('setTimeout'), [j.arrowFunctionExpression([], node), j.literal(timeout)]);
+		})
+		.toSource(printOptions);
+};
