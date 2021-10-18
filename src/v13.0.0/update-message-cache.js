3957a4eaf01ec54be5cb15b28911f01dab755ec1
Khasms <36800359+Khasms@users.noreply.github.com>
2021-10-17 21:38:44 -0700

    feat(13.0.0): add `messageCacheMaxSize` to `makeCache` transformer

---
+++ src/v13.0.0/update-message-cache.js
@@ -0,0 +1,45 @@
+const addImports = require('jscodeshift-add-imports');
+const { parser } = require('../util');
+
+module.exports = function transform(file, api, options) {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const printOptions = options.printOptions;
+
+	root
+		.find(j.ObjectExpression, {
+			properties: [
+				{
+					type: 'Property',
+					key: {
+						type: 'Identifier',
+						name: 'messageCacheMaxSize',
+					},
+				},
+			],
+		})
+		.forEach(({ node }) => {
+			const props = node.properties;
+			const maxSize = props.find((prop) => prop.key.name === 'messageCacheMaxSize');
+			const makeCache = j.property(
+				'init',
+				j.identifier('makeCache'),
+				j.callExpression(j.memberExpression(j.identifier('Options'), j.identifier('cacheWithLimits')), [
+					j.objectExpression([j.property('init', j.identifier('MessageManager'), maxSize.value)]),
+				]),
+			);
+
+			props.splice(props.indexOf(maxSize), 1);
+			props.push(makeCache);
+		});
+
+	addImports(
+		root,
+		j.importDeclaration([j.importSpecifier(j.identifier('Options'), j.identifier('Options'))], j.literal('discord.js')),
+	);
+
+	return root.toSource(printOptions);
+};
+
+module.exports.parser = parser;
